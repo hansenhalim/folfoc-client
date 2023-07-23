615f02d754e7ee79cbc2c68457a0b335719c35e0
V Hansen Halim <fpsecond.hh@gmail.com>
2023-07-23 15:38:19 +0700

    Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,26 @@
-#include "Arduino.h"
+#include <Arduino.h>
 #include <ESP32Encoder.h>
 
-#define CLK_PIN 23
-#define DT_PIN 22
-#define SW_PIN 21
+#define CLK 25 // CLK ENCODER
+#define DT 26  // DT ENCODER
 
 ESP32Encoder encoder;
-volatile int32_t newPosition = 0;
-int32_t oldPosition = 0;
-bool switchState = false;
-
-void IRAM_ATTR handleEncoder()
-{
-  if (digitalRead(CLK_PIN) == digitalRead(DT_PIN))
-  {
-    newPosition++;
-  }
-  else
-  {
-    newPosition--;
-  }
-}
 
 void setup()
 {
-  Serial.begin(9600);
-  encoder.attachSingleEdge(CLK_PIN, DT_PIN);
-  pinMode(SW_PIN, INPUT_PULLUP);
-  attachInterrupt(CLK_PIN, handleEncoder, CHANGE);
-  attachInterrupt(DT_PIN, handleEncoder, CHANGE);
+  encoder.attachSingleEdge(DT, CLK);
+  encoder.setCount(0);
+  Serial.begin(115200);
 }
 
 void loop()
 {
-  if (newPosition != oldPosition)
+  if (encoder.getCount() != 0)
   {
-    if (newPosition > oldPosition)
-    {
-      Serial.println("CW");
-    }
-    else
-    {
-      Serial.println("CCW");
-    }
-    oldPosition = newPosition;
+    if (encoder.getCount() > 0)
+      Serial.println("Clockwise");
+    else if (encoder.getCount() < 0)
+      Serial.println("Counterclockwise");
+    encoder.clearCount();
   }
-
-  if (digitalRead(SW_PIN) == LOW && !switchState)
-  {
-    Serial.println("Switch pressed");
-    switchState = true;
-  }
-  else if (digitalRead(SW_PIN) == HIGH)
-  {
-    switchState = false;
-  }
-
-  delay(100); // Adjust the delay time as needed
 }
