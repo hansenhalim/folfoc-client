d4d82f3f769058fed421a1cf39a9992bfd18f3ca
V Hansen Halim <fpsecond.hh@gmail.com>
2023-07-25 02:22:52 +0700

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <ESP32Encoder.h>
+#include <Wire.h>
 
 #define CLK 25 // CLK ENCODER
 #define DT 26  // DT ENCODER
@@ -10,7 +11,15 @@ void setup()
 {
   encoder.attachSingleEdge(DT, CLK);
   encoder.setCount(0);
-  Serial.begin(115200);
+  Wire.begin();
+}
+
+void sendCommand(uint8_t address, int motor_id, String direction, int steps)
+{
+  String command = String(motor_id) + "," + direction + "," + String(steps) + ";";
+  Wire.beginTransmission(address);
+  Wire.write(command.c_str());
+  Wire.endTransmission();
 }
 
 void loop()
@@ -18,9 +27,9 @@ void loop()
   if (encoder.getCount() != 0)
   {
     if (encoder.getCount() > 0)
-      Serial.println("Clockwise");
+      sendCommand(8, 1, "CW", 100); // 8 is the I2C address of Arduino A
     else if (encoder.getCount() < 0)
-      Serial.println("Counterclockwise");
+      sendCommand(8, 2, "CCW", 50); // 8 is the I2C address of Arduino A
     encoder.clearCount();
   }
 }
