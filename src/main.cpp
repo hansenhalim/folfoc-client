848397dd340e9c168a398c7f49239861938f2a25
V Hansen Halim <fpsecond.hh@gmail.com>
2023-07-25 04:02:53 +0700

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,17 @@
 #include <ESP32Encoder.h>
 #include <Wire.h>
 
+// Constants that the user passes in to the motor calls
+#define FORWARD 1
+#define BACKWARD 2
+
+#define MOTOR_A 0
+#define MOTOR_B 1
+
 #define CLK 25 // CLK ENCODER
 #define DT 26  // DT ENCODER
+
+#define I2C_ADDR 8 // The I2C address of server
 
 ESP32Encoder encoder;
 
@@ -14,9 +23,9 @@ void setup()
   Wire.begin();
 }
 
-void sendCommand(uint8_t address, int motor_id, String direction, int steps)
+void sendCommand(uint8_t address, uint8_t motor_id, uint8_t dir, uint16_t steps)
 {
-  String command = String(motor_id) + "," + direction + "," + String(steps) + ";";
+  String command = String(motor_id) + "," + String(dir) + "," + String(steps) + ";";
   Wire.beginTransmission(address);
   Wire.write(command.c_str());
   Wire.endTransmission();
@@ -26,10 +35,12 @@ void loop()
 {
   if (encoder.getCount() != 0)
   {
-    if (encoder.getCount() > 0)
-      sendCommand(8, 1, "CW", 100); // 8 is the I2C address of Arduino A
-    else if (encoder.getCount() < 0)
-      sendCommand(8, 2, "CCW", 50); // 8 is the I2C address of Arduino A
+    long long encoderCount = encoder.getCount();
+
+    if (encoderCount > 0) // Clockwise
+      sendCommand(I2C_ADDR, MOTOR_A, FORWARD, 100);
+    else if (encoderCount < 0) // Counterclockwise
+      sendCommand(I2C_ADDR, MOTOR_B, BACKWARD, 100);
     encoder.clearCount();
   }
 }
